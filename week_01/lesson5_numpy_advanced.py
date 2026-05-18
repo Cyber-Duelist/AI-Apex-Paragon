266491ad046cbad5607c284ce2afc82d9ae18bfb
Cyber-Duelist <ultraman5115@gmail.com>
2026-05-18 15:39:28 +0530

    week_01_lesson5_advanced_numpy

---
+++ week_01/lesson5_numpy_advanced.py
@@ -0,0 +1,8 @@
+import numpy as np
+matrix = np.array([[10,20,30],[40,50,60],[70,80,90]])
+print(matrix[0, 0])
+print(matrix[2, 1])
+print(matrix[1, 1])
+print(matrix[0:2,1:3])
+print(matrix[1:3, 1:3])
+print(matrix[0:2, 0:2])
