e1e7b9d7694ff2514b8c0064e35fbd2a94eb6205
Cyber-Duelist <ultraman5115@gmail.com>
2026-05-20 05:59:24 +0530

    week_01_lesson_08_pandas_basics

--- week_01/lesson8_pandas.py
+++ week_01/lesson8_pandas.py
@@ -13,3 +13,13 @@ print(data_df)
 memory_log = {"action": ["read_email", "open_browser", "type_notes"], "duration_seconds": [45,12,120]}
 entity_df = pd.DataFrame(memory_log)
 print(entity_df)
+print(entity_df["duration_seconds"].mean())
+print(entity_df["duration_seconds"].min())
+print(entity_df["duration_seconds"].max())
+print(entity_df.describe())
+
+slow_actions = entity_df[entity_df["duration_seconds"] > 50]
+print(slow_actions)
+
+fast_actions =entity_df[entity_df["duration_seconds"] < 50]
+print(fast_actions)
